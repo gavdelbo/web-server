8eaab760b5b10570484264c18521aebd8a558b41
Gavin D <gavdelbo@gmail.com>
2016-05-19 19:54:50 +1000

    Refactor middleware.

--- server.js
+++ server.js
@@ -2,19 +2,9 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!!');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: '+ new Date().toString() + req.method + ' ' + req.originalUrl );
-        next();   
-    }
-};
-app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
+var middleware = require('./middleware.js');
 
+app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function (req, res) {
     res.send('About Us');
 });
